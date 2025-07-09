91073c58d55eca68a69481002751ccea6744f4e2
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-07-09 20:45:03 +0300

    Fix migrations generator runtime err
    - remove view from typeorm runtime entities

--- src/shared/config/dataSource.ts
+++ src/shared/config/dataSource.ts
@@ -3,14 +3,7 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 
 import Env from '#src/shared/config/env';
-import {
-  CatDetails,
-  DogDetails,
-  FarmAnimalDetails,
-  HorseDetails,
-  Pet,
-  PetWithDetails,
-} from '#src/shared/entities';
+import { CatDetails, DogDetails, FarmAnimalDetails, HorseDetails, Pet } from '#src/shared/entities';
 import { paths } from '#src/shared/lib/constants';
 
 const { dbConfig, nodeEnv } = Env;
@@ -19,7 +12,7 @@ const { migrations } = paths;
 export const AppDataSource = new DataSource({
   type: 'postgres',
   ...dbConfig,
-  entities: [CatDetails, DogDetails, FarmAnimalDetails, HorseDetails, Pet, PetWithDetails],
+  entities: [CatDetails, DogDetails, FarmAnimalDetails, HorseDetails, Pet],
   logging: true,
   migrations: [nodeEnv === 'production' ? migrations.prod : migrations.local],
 });

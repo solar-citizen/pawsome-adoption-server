c1b57cfd348af7990fe3dd181f675f72a8f7b2e8
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-08-04 15:29:05 +0300

    Add tmp fix for req limiter

--- src/shared/middleware/rateLimit.ts
+++ src/shared/middleware/rateLimit.ts
@@ -1,10 +1,11 @@
 import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
-  // 5 minutes
-  windowMs: 5 * 60 * 1000,
+  // 1 minute
+  windowMs: 1 * 60 * 1000,
 
-  // Limit each IP to 100 requests per windowMs
-  limit: 100,
+  // Limit each IP to 150 requests per windowMs
+  // TODO: Find optimal limit rate for prod
+  limit: 500,
   message: 'Too many requests from this IP. Please try again later.',
 });
